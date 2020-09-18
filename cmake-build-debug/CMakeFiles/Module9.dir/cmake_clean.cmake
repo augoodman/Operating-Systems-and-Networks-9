068941f47ec014962cf23eb22bd9491397f39a2e
augoodma <augoodma@asu.edu>
2020-09-17 20:24:01 -0400

    first commit with project 2 as base code

---
+++ cmake-build-debug/CMakeFiles/Module9.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Module9.dir/main.c.o"
+  "Module9"
+  "Module9.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Module9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
